433705e277ef308da937c261ced11cc95b730ee5
Zach Hayes <hayes.Zach@protonmail.com>
2025-12-23 15:16:54 -0700

    add marketplace repository

---
+++ internal/repository/private/marketplace.go
@@ -0,0 +1,51 @@
+package private
+
+import (
+	"reverse-watch/internal/domain/models"
+	"reverse-watch/internal/domain/repository"
+
+	"gorm.io/gorm"
+)
+
+type marketplaceRepository struct {
+	conn *gorm.DB
+}
+
+var _ repository.MarketplaceRepository = (*marketplaceRepository)(nil)
+
+func NewMarketplaceRepository(conn *gorm.DB) repository.MarketplaceRepository {
+	return &marketplaceRepository{
+		conn: conn,
+	}
+}
+
+func (m *marketplaceRepository) Create(marketplace *models.Marketplace) error {
+	return m.conn.Create(marketplace).Error
+}
+
+func (m *marketplaceRepository) Read(slug string) (*models.Marketplace, error) {
+	var marketplace models.Marketplace
+	if err := m.conn.Model(&models.Marketplace{}).Where("slug = ?", slug).First(&marketplace).Error; err != nil {
+		return nil, err
+	}
+	return &marketplace, nil
+}
+
+func (m *marketplaceRepository) Update(slug string, fields map[string]interface{}) error {
+	return m.conn.Model(&models.Marketplace{}).Where("slug = ?", slug).Updates(fields).Error
+}
+
+func (m *marketplaceRepository) Delete(slug string) error {
+	return m.conn.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Model(&models.Key{}).Where("marketplace_slug = ?", slug).Delete(&models.Key{}).Error; err != nil {
+			return err
+		}
+		if err := tx.Model(&models.Marketplace{}).Where("slug = ?", slug).Delete(&models.Marketplace{}).Error; err != nil {
+			return err
+		}
+		if err := tx.Commit().Error; err != nil {
+			return err
+		}
+		return nil
+	})
+}
