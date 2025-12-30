19e63abfd3cedb53b11d446fe5d25a9d675b991a
Zach Hayes <hayes.Zach@protonmail.com>
2025-12-29 17:13:19 -0700

    fix error messages

--- internal/domain/dto/marketplace.go
+++ internal/domain/dto/marketplace.go
@@ -28,7 +28,7 @@ func (o *MarketplaceUpdateOptions) ToFields() map[string]interface{} {
 func (o *MarketplaceUpdateOptions) Validate() error {
 	if o.Slug != nil {
 		if len(*o.Slug) <= 0 || len(*o.Slug) > 25 {
-			return fmt.Errorf("slug must be between 0 and 25 characters long")
+			return fmt.Errorf("slug must be between 1 and 25 characters long")
 		}
 		if !regexp.MustCompile(`^[a-zA-Z0-9-]+$`).MatchString(*o.Slug) {
 			return fmt.Errorf("slug must contain only letters, numbers, and hyphens")
@@ -36,7 +36,7 @@ func (o *MarketplaceUpdateOptions) Validate() error {
 	}
 	if o.Name != nil {
 		if len(*o.Name) <= 0 || len(*o.Name) > 50 {
-			return fmt.Errorf("name must be between 0 and 50 characters long")
+			return fmt.Errorf("name must be between 1 and 50 characters long")
 		}
 	}
 	return nil
