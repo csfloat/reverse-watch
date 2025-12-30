63f4dc8c7dfd561a63058acb299fed5e945f55f0
Zach Hayes <hayes.Zach@protonmail.com>
2025-12-29 17:16:29 -0700

    remove ability to update slug

--- internal/domain/dto/marketplace.go
+++ internal/domain/dto/marketplace.go
@@ -2,20 +2,15 @@ package dto
 
 import (
 	"fmt"
-	"regexp"
 )
 
 type MarketplaceUpdateOptions struct {
-	Slug     *string
 	Name     *string
 	IsActive *bool
 }
 
 func (o *MarketplaceUpdateOptions) ToFields() map[string]interface{} {
 	fields := make(map[string]interface{})
-	if o.Slug != nil {
-		fields["slug"] = *o.Slug
-	}
 	if o.Name != nil {
 		fields["name"] = *o.Name
 	}
@@ -26,14 +21,6 @@ func (o *MarketplaceUpdateOptions) ToFields() map[string]interface{} {
 }
 
 func (o *MarketplaceUpdateOptions) Validate() error {
-	if o.Slug != nil {
-		if len(*o.Slug) <= 0 || len(*o.Slug) > 25 {
-			return fmt.Errorf("slug must be between 1 and 25 characters long")
-		}
-		if !regexp.MustCompile(`^[a-zA-Z0-9-]+$`).MatchString(*o.Slug) {
-			return fmt.Errorf("slug must contain only letters, numbers, and hyphens")
-		}
-	}
 	if o.Name != nil {
 		if len(*o.Name) <= 0 || len(*o.Name) > 50 {
 			return fmt.Errorf("name must be between 1 and 50 characters long")
