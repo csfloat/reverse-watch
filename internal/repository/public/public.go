fbfff6fa7b852113eccf785a15ecf7b57e9ade65
Zach Hayes <hayes.Zach@protonmail.com>
2026-01-22 14:58:39 -0700

    update index

--- internal/repository/public/public.go
+++ internal/repository/public/public.go
@@ -125,7 +125,7 @@ func createIndexes(tx *gorm.DB) error {
 	indexes := []string{
 		`CREATE UNIQUE INDEX IF NOT EXISTS idx_reversals_steam_id_marketplace_slug ON reversals(steam_id, marketplace_slug)`,
 		`CREATE INDEX IF NOT EXISTS idx_reversals_marketplace_slug ON reversals(marketplace_slug)`,
-		`CREATE INDEX IF NOT EXISTS idx_reversals_created_at_desc ON reversals(created_at DESC, id DESC)`,
+		`CREATE INDEX IF NOT EXISTS idx_reversals_id_desc ON reversals(id DESC)`,
 	}
 
 	for _, index := range indexes {
