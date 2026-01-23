495fa3b63b0cc74ba2d4167e74a284e2ab3643d5
Zach Hayes <hayes.Zach@protonmail.com>
2026-01-23 16:32:45 -0700

    fix param name

--- internal/domain/service/reversal.go
+++ internal/domain/service/reversal.go
@@ -9,7 +9,7 @@ type ReversalService interface {
 	CreateReversal(reversal *models.Reversal) error
 	BulkCreateReversals(reversals []*models.Reversal) error
 	GetReversal(id models.Snowflake) (*models.Reversal, error)
-	UpdateReversal(id models.Snowflake, opts *dto.ReversalUpdates) error
+	UpdateReversal(id models.Snowflake, updates *dto.ReversalUpdates) error
 	DeleteReversal(id models.Snowflake) error
 	ListReversals(opts *dto.ReversalListOptions) ([]*models.Reversal, error)
 }
