84a0df58d81101a5977c72e772f848f711051fa1
Zach Hayes <hayes.Zach@protonmail.com>
2026-01-24 13:43:32 -0700

    add validate tests

--- internal/domain/dto/marketplace_test.go
+++ internal/domain/dto/marketplace_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"reverse-watch/internal/domain/models"
+	"reverse-watch/internal/util"
 )
 
 func TestMarketplaceUpdates_FieldCoverage(t *testing.T) {
@@ -47,5 +48,60 @@ func TestMarketplaceUpdates_FieldCoverage(t *testing.T) {
 		if updatesField.Type.Kind() == reflect.Ptr && marketplaceField.Type != updatesField.Type.Elem() {
 			t.Errorf("MarketplaceUpdates contains field %q with incorrect type", updatesField.Name)
 		}
+	}
+}
+
+func TestMarketplaceUpdates_Validate(t *testing.T) {
+	t.Parallel()
+
+	updates := &MarketplaceUpdates{
+		Name:     util.Ptr("Test Marketplace"),
+		IsActive: util.Ptr(true),
+	}
+
+	if err := updates.Validate(); err != nil {
+		t.Errorf("Validate(): %v", err)
+	}
+}
+
+func TestMarketplaceUpdates_Validate_Errors(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name    string
+		updates *MarketplaceUpdates
+		wantErr string
+	}{
+		{
+			name:    "emptyUpdates",
+			updates: &MarketplaceUpdates{},
+			wantErr: "marketplace updates must have at least one field",
+		},
+		{
+			name: "emptyName",
+			updates: &MarketplaceUpdates{
+				Name: util.Ptr(""),
+			},
+			wantErr: "name must be between 1 and 50 characters long",
+		},
+		{
+			name: "nameTooLong",
+			updates: &MarketplaceUpdates{
+				Name: util.Ptr("Test Marketplace this name is invalid since it is too long"),
+			},
+			wantErr: "name must be between 1 and 50 characters long",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.updates.Validate()
+			if err == nil {
+				t.Errorf("Validate(): got nil error, want %s", tc.wantErr)
+			}
+			if err.Error() != tc.wantErr {
+				t.Errorf("Validate(): got error %s, want %s", err.Error(), tc.wantErr)
+			}
+		})
 	}
 }
