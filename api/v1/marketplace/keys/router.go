df8e9126fd7b6511218d6630f78570e5ae65b553
Zach Hayes <hayes.Zach@protonmail.com>
2026-01-30 16:10:54 -0700

    require manage permission

--- api/v1/marketplace/keys/router.go
+++ api/v1/marketplace/keys/router.go
@@ -1,11 +1,15 @@
 package keys
 
 import (
+	"reverse-watch/domain/models"
+	rwmiddleware "reverse-watch/middleware"
+
 	"github.com/go-chi/chi/v5"
 )
 
 func Router() chi.Router {
 	r := chi.NewRouter()
+	r.Use(rwmiddleware.RequirePermissions(models.PermissionManage))
 	r.Post("/", createKey)
 	return r
 }
