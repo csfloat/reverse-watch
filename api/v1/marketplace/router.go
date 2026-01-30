2e9c919aafb198da3503f270d9dcb1f0756d4e0f
Zach Hayes <hayes.Zach@protonmail.com>
2026-01-30 16:10:46 -0700

    update auth middleware

--- api/v1/marketplace/router.go
+++ api/v1/marketplace/router.go
@@ -2,12 +2,14 @@ package marketplace
 
 import (
 	"reverse-watch/api/v1/marketplace/keys"
+	rwmiddleware "reverse-watch/middleware"
 
 	"github.com/go-chi/chi/v5"
 )
 
 func Router() chi.Router {
 	r := chi.NewRouter()
+	r.Use(rwmiddleware.AuthMiddleware)
 	r.Mount("/keys", keys.Router())
 	return r
 }
