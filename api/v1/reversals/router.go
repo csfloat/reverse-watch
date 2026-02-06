6bd8f11fd2a00d180c4005ac29ac20b9a62ba8c6
Zach Hayes <hayes.Zach@protonmail.com>
2026-02-06 13:14:18 -0700

    add create reversals handler

---
+++ api/v1/reversals/router.go
@@ -0,0 +1,9 @@
+package reversals
+
+import "github.com/go-chi/chi/v5"
+
+func Router() chi.Router {
+	r := chi.NewRouter()
+	r.Post("/", createReversal)
+	return r
+}
--- api/v1/v1.go
+++ api/v1/v1.go
@@ -2,6 +2,7 @@ package v1
 
 import (
 	"reverse-watch/api/v1/marketplace"
+	"reverse-watch/api/v1/reversals"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -9,5 +10,6 @@ import (
 func Router() chi.Router {
 	r := chi.NewRouter()
 	r.Mount("/marketplace", marketplace.Router())
+	r.Mount("/reversals", reversals.Router())
 	return r
 }
