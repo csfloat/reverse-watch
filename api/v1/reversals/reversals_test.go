6c87d6e515d04d4d7de46e760715bb8a4cbb810e
Zach Hayes <hayes.Zach@protonmail.com>
2026-02-06 14:29:08 -0700

    test creating multiple reversals

--- api/v1/reversals/reversals_test.go
+++ api/v1/reversals/reversals_test.go
@@ -119,6 +119,99 @@ func TestCreateReversal(t *testing.T) {
 				}
 			},
 		},
+		{
+			name: "multipleReversals",
+			setup: func(db *gorm.DB, f repository.Factory, keygen isecret.KeyGenerator) (*http.Request, []*reversal, error) {
+				testMarketplace := &models.Marketplace{
+					Slug:     "test-marketplace",
+					Name:     "Test Marketplace",
+					IsActive: true,
+				}
+				testutil.Insert(t, db, testMarketplace)
+
+				testKey, err := keygen.GenerateSecretKey()
+				if err != nil {
+					return nil, nil, err
+				}
+
+				id, err := testKey.ID()
+				if err != nil {
+					return nil, nil, err
+				}
+
+				key := &models.Key{
+					ID:              id,
+					Environment:     keygen.Environment(),
+					MarketplaceSlug: testMarketplace.Slug,
+					Permissions:     models.PermissionWrite,
+				}
+				testutil.Insert(t, db, key)
+
+				data := []*reversal{
+					{
+						SteamID:        models.SteamID(76561197960287930),
+						Source:         util.Ptr(models.SourceRelatedUser),
+						RelatedSteamID: util.Ptr(models.SteamID(76561197960287931)),
+						ReversedAt:     1717756800,
+					},
+					{
+						SteamID:    models.SteamID(76561197960287931),
+						ReversedAt: 1717756801,
+					},
+					{
+						SteamID:    models.SteamID(76561197960287932),
+						Source:     util.Ptr(models.SourceDirect),
+						ReversedAt: 1717756802,
+					},
+					{
+						SteamID:    models.SteamID(76561197960287933),
+						Source:     util.Ptr(models.SourceUserReport),
+						ReversedAt: 1717756803,
+					},
+				}
+
+				body, err := json.Marshal(&req{Data: data})
+				if err != nil {
+					return nil, nil, err
+				}
+
+				formattedKey, err := testKey.Format()
+				if err != nil {
+					return nil, nil, err
+				}
+
+				r := httptest.NewRequest(http.MethodPost, "/", bytes.NewBuffer(body))
+				r.Header.Set("Content-Type", "application/json")
+				r.Header.Set("Authorization", "Bearer "+formattedKey)
+				return r, data, nil
+			},
+			validateFunc: func(t *testing.T, db *gorm.DB, data []*reversal, resp *http.Response) {
+				if resp.StatusCode != http.StatusOK {
+					t.Errorf("got status code %d, wanted %d", resp.StatusCode, http.StatusOK)
+				}
+
+				defer resp.Body.Close()
+				var respData []*models.Reversal
+				if err := json.NewDecoder(resp.Body).Decode(&respData); err != nil {
+					t.Fatalf("failed to decode response body: %v", err)
+				}
+
+				reversals := make([]*models.Reversal, 0)
+				for _, reversal := range data {
+					reversals = append(reversals, &models.Reversal{
+						SteamID:         reversal.SteamID,
+						MarketplaceSlug: "test-marketplace",
+						Source:          reversal.Source,
+						RelatedSteamID:  reversal.RelatedSteamID,
+						ReversedAt:      reversal.ReversedAt,
+					})
+				}
+
+				if diff := cmp.Diff(respData, reversals, cmpopts.IgnoreFields(models.Reversal{}, "ID", "CreatedAt", "UpdatedAt", "MarketplaceSlug")); diff != "" {
+					t.Error(diff)
+				}
+			},
+		},
 	}
 
 	for _, tc := range testCases {
@@ -136,7 +229,7 @@ func TestCreateReversal(t *testing.T) {
 
 			factoryMiddleware := middleware.FactoryMiddleware(f)
 			permissionsMiddleware := middleware.RequirePermissions(models.PermissionWrite)
-			handler := http.HandlerFunc(createReversal)
+			handler := http.HandlerFunc(createReversals)
 
 			finalHandler := factoryMiddleware(
 				middleware.AuthMiddleware(
