05d5d0974e34a19b2cafb12f36adc6d828735806
Zach Hayes <hayes.Zach@protonmail.com>
2026-02-10 15:38:00 -0700

    wire up expunge reversal handler

--- api/v1/reversals/router.go
+++ api/v1/reversals/router.go
@@ -12,6 +12,7 @@ func Router() chi.Router {
 	r.Use(middleware.AuthMiddleware)
 
 	r.With(middleware.RequirePermissions(models.PermissionWrite)).Post("/", createReversals)
+	r.With(middleware.RequirePermissions(models.PermissionDelete)).Delete("/{id}", expungeReversal)
 	r.With(middleware.RequirePermissions(models.PermissionExport)).Route("/", func(r chi.Router) {
 		r.Get("/", listReversalsHandler)
 		r.Get("/export", exportReversals)
