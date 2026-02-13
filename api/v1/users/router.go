cd0331b46536729e9ddbe2520e9950eeaf556378
Zach Hayes <hayes.Zach@protonmail.com>
2026-02-12 18:23:32 -0700

    decrease rate limiting

--- api/v1/users/router.go
+++ api/v1/users/router.go
@@ -10,6 +10,6 @@ import (
 
 func Router() chi.Router {
 	r := chi.NewRouter()
-	r.With(ratelimit.ThrottleByIP(time.Minute, 15)).Get("/{steamId}", fetchUserStatus)
+	r.With(ratelimit.ThrottleByIP(time.Minute, 100)).Get("/{steamId}", fetchUserStatus)
 	return r
 }
