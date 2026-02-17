531e6849b3e59b9cf232123357bab051b3b2c79e
hayesZach <hayes.Zach@protonmail.com>
2026-02-17 10:28:00 -0700

    address comments

--- api/v1/admin/marketplace/router.go
+++ api/v1/admin/marketplace/router.go
@@ -3,8 +3,6 @@ package marketplace
 import (
 	"time"
 
-	"reverse-watch/domain/models"
-	"reverse-watch/middleware"
 	"reverse-watch/ratelimit"
 
 	"github.com/go-chi/chi/v5"
@@ -12,8 +10,6 @@ import (
 
 func Router() chi.Router {
 	r := chi.NewRouter()
-	r.Use(middleware.AuthMiddleware)
-	r.Use(middleware.RequirePermissions(models.PermissionAdmin))
 	r.Use(ratelimit.ThrottleByAPIKey(time.Hour, 2_000))
 
 	r.Post("/", onboardMarketplace)
