d80d474cbeb688426c7b12d80c5c3f9e7333a4b5
Zach Hayes <hayes.Zach@protonmail.com>
2026-03-09 12:10:01 -0700

    pull config from env in prod

--- config/config.go
+++ config/config.go
@@ -63,6 +63,9 @@ func load() Config {
 		},
 	))
 
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	v.AutomaticEnv()
+
 	v.SetDefault("Database.Host", "localhost")
 	v.SetDefault("Database.Port", "5432")
 	v.SetDefault("Database.User", "postgres")
@@ -76,17 +79,20 @@ func load() Config {
 	v.SetDefault("Ingestors.CSFloat.Enable", false)
 	v.SetDefault("Ingestors.CSFloat.BaseURL", "https://csfloat.com")
 
+	v.BindEnv("Ingestors.CSFloat.SecretKey")
+
+	// Try to find the root directory, but don't panic if it fails since go.mod doesn't exist in production
 	dir, err := GetProjectRootDir()
-	if err != nil {
-		panic(err)
-	}
+	if err == nil {
+		v.AddConfigPath(dir)
+		v.SetConfigName("config")
+		v.SetConfigType("json")
 
-	v.AddConfigPath(dir)
-	v.SetConfigName("config")
-	v.SetConfigType("json")
-
-	if err := v.ReadInConfig(); err != nil {
-		panic(err)
+		if err := v.ReadInConfig(); err != nil {
+			if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+				panic(err)
+			}
+		}
 	}
 
 	var cfg Config
