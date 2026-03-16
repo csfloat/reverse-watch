16683bc6ab462e9f93d9500b37256d87b54b63b1
Zach Hayes <hayes.Zach@protonmail.com>
2026-03-16 11:51:19 -0700

    add BindEnv for AllowedOrigins

--- config/config.go
+++ config/config.go
@@ -83,6 +83,7 @@ func load() Config {
 	v.SetDefault("Ingestors.CSFloat.BaseURL", "https://csfloat.com")
 
 	// Need to register environment variables if defaults aren't set
+	v.BindEnv("HTTP.AllowedOrigins")
 	v.BindEnv("Ingestors.CSFloat.SecretKey")
 
 	// Try to find the root directory, but don't panic if it fails since go.mod doesn't exist in production
